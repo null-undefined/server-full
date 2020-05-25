d0d7e1056307e74a1e42a4b07b94e50e1ec61885
null-undefined <thiaguinhols144@gmail.com>
2020-05-25 17:16:04 -0300

    Added access log

--- src/app.js
+++ src/app.js
@@ -2,9 +2,39 @@
 
 require('dotenv').config()
 
+const path = require('path')
+
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const rfs = require('rotating-file-stream')
 
 const app = express()
 app.set('port', process.env.PORT || 5000)
+
+app.use(express.json())
+app.use(cors())
+if (app.get('env') === 'development') {
+	app.use(morgan('dev'))
+} else {
+	const accessLogStream = rfs.createStream('access.log', {
+		size: '10M',
+		interval: '3d',
+		path: path.resolve(__dirname, '..', 'logs')
+	})
+	app.use(morgan('combined', { stream: accessLogStream }))
+}
+
+app.use('*', async (req, res, next) => {
+	res.status(404)
+	next(new Error('page not found'))
+})
+
+app.use(async (err, req, res, next) => {
+	const statusCode = res.statusCode || 500
+	res
+		.status(statusCode)
+		.json({ error: err.message })
+})
 
 module.exports = app
--- tests/index.js
+++ tests/index.js
@@ -9,5 +9,8 @@ describe('index route', async () => {
 	it('status code page not found', async () => {
 		const response = await request(app).get('/')
 		expect(response.statusCode).to.equal(404)
+		expect(response.body).to.be.an('object')
+		expect(response.body).to.have.property('error')
+		expect(response.body).to.deep.equal({ error: 'page not found' })
 	})
 })
